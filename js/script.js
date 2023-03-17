10404705fa4c849de255ef22079f0984116bb7ba
Sceanav <sceanav@gmail.com>
2023-03-17 13:35:32 +0100

    final

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 //https://www.youtube.com/watch?v=lhNdUVh3qCc&t=115s
 
 document.addEventListener("DOMContentLoaded", () => {
+  //Se dispara antes de que el html haya acabado de cargar
   const cardArray = [
     { name: "brocoli", img: "img/brocoli.png" },
     { name: "brocoli", img: "img/brocoli.png" },
@@ -21,12 +22,15 @@ document.addEventListener("DOMContentLoaded", () => {
   ];
 });
 
+cardArray.sort(() => 0.5 - Math.random());
+
 const grid = document.querySelector(".grid)");
 /* asignamos a nestra constante grid la clase .grid
  que corresponde a nuestro div */
-
+const resultDisplay = document.querySelector("#result");
 var cardsChosen = []; //Es un arrayd e nombre de cartas
-var cardsChosenId = [];
+var cardsChosenId = []; //Array con el ide de cada casilla
+var cardsWon = []; // Array de pareja de cartas acertadas
 
 /* Creamos el tablero */
 function createBoard() {
@@ -39,11 +43,35 @@ function createBoard() {
   }
 }
 
+function checkForMatch() {
+  var cards = document.querySelectorAll("img");
+  const optionOneId = cardsChosenId[0];
+  const optionTwoId = cardsChosenId[1];
+
+  //Comprobar si dos cartas son iguales
+  if (cardsChosen[0] === cardsChosen[1]) {
+    alert("Coincidencia Encontrada");
+    cards[optionOneId].setAttribute("src", "img/white.png"); //Si las cartas son iguales, las substitue por white
+    cards[optionTwoId].setAttribute("src", "img/white.png");
+  } else {
+    alert("Eres una patata con patas");
+    cards[optionOneId].setAttribute("src", "img/blank.png"); //Si las cartas son dieferentes, les doy la vuelta.
+    cards[optionTwoId].setAttribute("src", "img/blank.png");
+  }
+  cardsChosen = [];
+  cardsChosenId = [];
+  resultDisplay.textContent = cardsWon.length;
+  if (cardsWon.length === cardArray.length / 2) {
+    resultDisplay.textContent = "FELICIDADES, NO ERES TAN IDIOTA COMO PARECÍAS";
+  }
+}
+
 function flipCard() {
   var cardId = this.getAttribute("data-id"); //En qué casilla he hecho clic
   cardsChosen.push(cardArray[cardId].name);
+  cardsChosenId.push(cardId);
   this.setAttribute("src", cardArray[cardId].img); // Doy la vuelta a la carta elegida, ya no muestro blank sino su contenido
   if (cardsChosen.length === 2) {
-    setTimeout(checkForMatch, 500); //Si he escogido 2 cartas, compruebo si hay coincidencia o no
+    setTimeout(checkForMatch, 500); //Si ya he escogido 2 cartas, compruebo si hay coincidencia o no
   }
 }
