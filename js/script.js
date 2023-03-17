a3aee1f912ecbd48aa09b6bcc9e7eb570218815d
Sceanav <sceanav@gmail.com>
2023-03-17 14:14:23 +0100

    copiado

--- js/script.js
+++ js/script.js
@@ -20,60 +20,127 @@ document.addEventListener("DOMContentLoaded", () => {
     { name: "pizza", img: "img/pizza.png" },
     { name: "pizza", img: "img/pizza.png" },
   ];
+
+  cardArray.sort(() => 0.5 - Math.random());
+
+  const grid = document.querySelector(".grid");
+  const resultDisplay = document.querySelector("#result");
+  let cardsChosen = [];
+  let cardsChosenId = [];
+  let cardsWon = [];
+
+  //create your board
+  function createBoard() {
+    for (let i = 0; i < cardArray.length; i++) {
+      const card = document.createElement("img");
+      card.setAttribute("src", "img/blank.png");
+      card.setAttribute("data-id", i);
+      card.addEventListener("click", flipCard);
+      grid.appendChild(card);
+    }
+  }
+
+  //check for matches
+  function checkForMatch() {
+    const cards = document.querySelectorAll("img");
+    const optionOneId = cardsChosenId[0];
+    const optionTwoId = cardsChosenId[1];
+
+    if (optionOneId == optionTwoId) {
+      cards[optionOneId].setAttribute("src", "img/blank.png");
+      cards[optionTwoId].setAttribute("src", "img/blank.png");
+      alert("You have clicked the same image!");
+    } else if (cardsChosen[0] === cardsChosen[1]) {
+      alert("You found a match");
+      cards[optionOneId].setAttribute("src", "img/white.png");
+      cards[optionTwoId].setAttribute("src", "img/white.png");
+      cards[optionOneId].removeEventListener("click", flipCard);
+      cards[optionTwoId].removeEventListener("click", flipCard);
+      cardsWon.push(cardsChosen);
+    } else {
+      cards[optionOneId].setAttribute("src", "img/blank.png");
+      cards[optionTwoId].setAttribute("src", "img/blank.png");
+      alert("Sorry, try again");
+    }
+    cardsChosen = [];
+    cardsChosenId = [];
+    resultDisplay.textContent = cardsWon.length;
+    if (cardsWon.length === cardArray.length / 2) {
+      resultDisplay.textContent = "Congratulations! You found them all!";
+    }
+  }
+
+  //flip your card
+  function flipCard() {
+    let cardId = this.getAttribute("data-id");
+    cardsChosen.push(cardArray[cardId].name);
+    cardsChosenId.push(cardId);
+    this.setAttribute("src", cardArray[cardId].img);
+    if (cardsChosen.length === 2) {
+      setTimeout(checkForMatch, 500);
+    }
+  }
+
+  createBoard();
 });
 
-cardArray.sort(() => 0.5 - Math.random());
+/*
+  cardArray.sort(() => 0.5 - Math.random());
 
-const grid =
-  document.querySelector(
-    ".grid"
-  ); /* asignamos a nuestra constante grid la clase .grid
- que corresponde a nuestro div */
-const resultDisplay = document.querySelector("#result");
-var cardsChosen = []; //Es un arrayd e nombre de cartas
-var cardsChosenId = []; //Array con el ide de cada casilla
-var cardsWon = []; // Array de pareja de cartas acertadas
+  const grid =
+    document.querySelector(
+      ".grid"
+    ); /* asignamos a nuestra constante grid la clase .grid
+ que corresponde a nuestro div 
+  const resultDisplay = document.querySelector("#result");
+  var cardsChosen = []; //Es un arrayd e nombre de cartas
+  var cardsChosenId = []; //Array con el ide de cada casilla
+  var cardsWon = []; // Array de pareja de cartas acertadas
 
-/* Creamos el tablero */
-function createBoard() {
-  for (let i = 0; i < cardArray.length(); i++) {
-    var card = document.createElement("img");
-    card.setAttribute("src", "img/blank.png");
-    card.setAttribute("data-id", i);
-    card.addEventListener("click", flipCard); //pongo una escucha en cada casilla
-    grid.appendChild(card);
+  /* Creamos el tablero 
+  function createBoard() {
+    for (let i = 0; i < cardArray.length(); i++) {
+      alert(i);
+      var card = document.createElement("img");
+      card.setAttribute("src", "img/blank.png");
+      card.setAttribute("data-id", i);
+      card.addEventListener("click", flipCard); //pongo una escucha en cada casilla
+      grid.appendChild(card);
+    }
   }
-}
 
-function checkForMatch() {
-  var cards = document.querySelectorAll("img");
-  const optionOneId = cardsChosenId[0];
-  const optionTwoId = cardsChosenId[1];
+  function checkForMatch() {
+    var cards = document.querySelectorAll("img");
+    const optionOneId = cardsChosenId[0];
+    const optionTwoId = cardsChosenId[1];
 
-  //Comprobar si dos cartas son iguales
-  if (cardsChosen[0] === cardsChosen[1]) {
-    alert("Coincidencia Encontrada");
-    cards[optionOneId].setAttribute("src", "img/white.png"); //Si las cartas son iguales, las substitue por white
-    cards[optionTwoId].setAttribute("src", "img/white.png");
-  } else {
-    alert("Eres una patata con patas");
-    cards[optionOneId].setAttribute("src", "img/blank.png"); //Si las cartas son dieferentes, les doy la vuelta.
-    cards[optionTwoId].setAttribute("src", "img/blank.png");
+    //Comprobar si dos cartas son iguales
+    if (cardsChosen[0] === cardsChosen[1]) {
+      alert("Coincidencia Encontrada");
+      cards[optionOneId].setAttribute("src", "img/white.png"); //Si las cartas son iguales, las substitue por white
+      cards[optionTwoId].setAttribute("src", "img/white.png");
+    } else {
+      alert("Eres una patata con patas");
+      cards[optionOneId].setAttribute("src", "img/blank.png"); //Si las cartas son dieferentes, les doy la vuelta.
+      cards[optionTwoId].setAttribute("src", "img/blank.png");
+    }
+    cardsChosen = [];
+    cardsChosenId = [];
+    resultDisplay.textContent = cardsWon.length;
+    if (cardsWon.length === cardArray.length / 2) {
+      resultDisplay.textContent =
+        "FELICIDADES, NO ERES TAN IDIOTA COMO PARECÍAS";
+    }
   }
-  cardsChosen = [];
-  cardsChosenId = [];
-  resultDisplay.textContent = cardsWon.length;
-  if (cardsWon.length === cardArray.length / 2) {
-    resultDisplay.textContent = "FELICIDADES, NO ERES TAN IDIOTA COMO PARECÍAS";
-  }
-}
 
-function flipCard() {
-  var cardId = this.getAttribute("data-id"); //En qué casilla he hecho clic
-  cardsChosen.push(cardArray[cardId].name);
-  cardsChosenId.push(cardId);
-  this.setAttribute("src", cardArray[cardId].img); // Doy la vuelta a la carta elegida, ya no muestro blank sino su contenido
-  if (cardsChosen.length === 2) {
-    setTimeout(checkForMatch, 500); //Si ya he escogido 2 cartas, compruebo si hay coincidencia o no
+  function flipCard() {
+    var cardId = this.getAttribute("data-id"); //En qué casilla he hecho clic
+    cardsChosen.push(cardArray[cardId].name);
+    cardsChosenId.push(cardId);
+    this.setAttribute("src", cardArray[cardId].img); // Doy la vuelta a la carta elegida, ya no muestro blank sino su contenido
+    if (cardsChosen.length === 2) {
+      setTimeout(checkForMatch, 500); //Si ya he escogido 2 cartas, compruebo si hay coincidencia o no
+    }
   }
-}
+  createBoard();
+}); */
